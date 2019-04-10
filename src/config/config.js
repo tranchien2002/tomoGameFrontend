36d2da3342b64cb80d1a5322dde0db0b0af14aa2
hoang <vinhyenvodoi98@gmail.com>
2019-04-10 18:47:53 +0700

    try add firebasestore

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
-import firebase from 'f'
-
-
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 var config = {
     apiKey: "AIzaSyCAtg8AwpPNfSCchbGf6m9iivQ8Q3H5XOs",
@@ -10,4 +10,7 @@ var config = {
     storageBucket: "hunter-31765.appspot.com",
     messagingSenderId: "633708017779"
   };
-  firebase.initializeApp(config);
+  
+firebase.initializeApp(config);
+
+export default firebase;
