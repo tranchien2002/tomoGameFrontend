a1c067a7ed81b92fb98f4da0d05bbadc2da35a15
hoang <vinhyenvodoi98@gmail.com>
2019-04-10 18:46:23 +0700

    add action

---
+++ src/store/action/quesAction.js
@@ -0,0 +1,6 @@
+export const createQues = (ques) =>{
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        //make async to database
+        dispatch({ type : 'GET_QUES', ques})
+    }
+};
