8e8a3991cde5e99af58dbce17a411509ca0b6913
hoang <vinhyenvodoi98@gmail.com>
2019-04-10 15:59:32 +0700

    firebase config

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+import firebase from 'f'
+
+
+
+var config = {
+    apiKey: "AIzaSyCAtg8AwpPNfSCchbGf6m9iivQ8Q3H5XOs",
+    authDomain: "hunter-31765.firebaseapp.com",
+    databaseURL: "https://hunter-31765.firebaseio.com",
+    projectId: "hunter-31765",
+    storageBucket: "hunter-31765.appspot.com",
+    messagingSenderId: "633708017779"
+  };
+  firebase.initializeApp(config);
