9300a4f6efb4ea6e3062d45a4013ba4629503034
hoang <vinhyenvodoi98@gmail.com>
2019-04-17 14:42:17 +0700

    add score-board

---
+++ src/component/PlayerLayout/ScoreBoard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css'
+
+const ScoreBoard = () => {
+    return(
+        <div className = "score_board">
+            <h2>Your Score</h2>
+            <h3>4/10</h3>
+        </div>
+
+    )
+
+}
+
+export default ScoreBoard;
