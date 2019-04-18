53a2954521fc8390e171351d096d9bd70f4e326a
hoang <vinhyenvodoi98@gmail.com>
2019-04-18 11:08:04 +0700

    admin-select-question

--- src/component/AdminLayout/AdminLayout.js
+++ src/component/AdminLayout/AdminLayout.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
 import { Container, Col, Row, Button} from 'reactstrap'
 // import '../../style/admin.css';
-import Adminselectques from './Adminselectques'
-import { connect } from 'react-redux'
-import { firestoreConnect } from 'react-redux-firebase'
-import { compose } from 'redux'
+import * as actions from '../../actions/insertquesAction'
+import store from '../../store';
+import firebase from '../../config/config'
 import '../App.css'
 import '../../style/admin.css'
 
@@ -14,14 +13,36 @@ class AdminLayout extends Component {
     constructor(props){
         super(props)
         this.state ={
-            account : '0x0',
-            balance : '',
+            selected : [],
+            question : [],
         }
     }
-    
-    render() {   
-        const {question} =this.props;
+
+    async componentWillMount(){
+        var db = await firebase.firestore();
+        db.collection("project_hunter").get().then((querySnapshot) =>{
+            querySnapshot.forEach((doc) => {
+                this.setState({question : [...this.state.question.concat([doc.data()])]})
+            });
+        });
+    }   
+
+    selectQues = async (ques) =>{
+        await this.setState({selected : [...this.state.selected.concat([ques.quesNumber])]});
         
+        let found = await this.state.selected.find((element) =>{
+            return element === ques.quesNumber
+        })
+        
+        // console.log(this.state.question)
+        await delete this.state.question[found];
+        this.setState(this.state)
+
+        store.dispatch(actions.insertQues(ques));
+    }
+
+    render() {   
+        const {question} =this.state; 
         return (   
             <div>
                 <Container>
@@ -32,11 +53,13 @@ class AdminLayout extends Component {
                             </div>
                             <div className="admin_question_box">
                                 {
-                                    question && question.map(ques => (
-                                        <div className = "admin_quesbox" key = {ques.id}>
-                                            <Adminselectques ques ={ques.question}/> 
-                                        </div>                                
-                                ))}
+                                    question && question.map(ques => {
+                                        return(
+                                            <div className = "admin_quesbox" key = {ques.quesNumber}>
+                                                <Button onClick={() => this.selectQues(ques)} className = "answer_box " outline color="primary" >{ques.question}</Button>  
+                                            </div> 
+                                        )}
+                                )}
                             </div>
                             <div className ="button_bounty">
                                 <Row>
@@ -56,20 +79,4 @@ class AdminLayout extends Component {
     }
 }
 
-const mapStatetoProps = (state) => {
-    // console.log(state.ques.questionID[0])
-    return {
-        question : state.firestore.ordered.project_hunter,
-    }
-}
-
-export default compose(
-    connect(mapStatetoProps),
-    firestoreConnect([
-        { 
-            collection : 'project_hunter',
-        }
-    ])
-)(AdminLayout);
-
-// export default AdminLayout;
+export default AdminLayout;
