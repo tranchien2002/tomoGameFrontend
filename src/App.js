c7a3e015e664cff7f1bcb3cc45beb18ed7ff419a
Do Trung Kien <trungkien.keio@gmail.com>
2019-06-20 16:20:45 +0700

    refactor js codes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import PlayerLayout from './component/PlayerLayout/PlayerLayout'
-import AdminLayout from './component/AdminLayout/AdminLayout'
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import PlayerLayout from './component/PlayerLayout/PlayerLayout';
+import AdminLayout from './component/AdminLayout/AdminLayout';
 
 class App extends Component {
-    
-    render() {
-        return (
-          <BrowserRouter>
-            <div className="App">
-              <Switch>
-                <Route exact path='/'component={PlayerLayout} />
-                <Route path='/admin' component={AdminLayout} />
-              </Switch>
-            </div>
-          </BrowserRouter>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <div className='App'>
+          <Switch>
+            <Route exact path='/' component={PlayerLayout} />
+            <Route path='/admin' component={AdminLayout} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/component/PlayerLayout/ScoreBoard.js
+++ src/component/PlayerLayout/ScoreBoard.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import '../App.css'
+import '../App.css';
 
-const ScoreBoard = ({wincount}) => {
-    return(
-        <div className = "score_board">
-            <h2>Your Score</h2>
-            <h3>{wincount}/10</h3>
-        </div>
-
-    )
-
-}
+const ScoreBoard = ({ wincount }) => {
+  return (
+    <div className='score_board'>
+      <h2>Your Score</h2>
+      <h3>{wincount}/10</h3>
+    </div>
+  );
+};
 
 export default ScoreBoard;
