ff4ac2f172bf82c9f1d8f27c675ec8c19838c23a
le.thanh.cong <SUN-ASTERISK\le.thanh.cong@i120917-pc.sun-asterisk.com>
2019-07-16 11:57:46 +0700

    change background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import PlayerLayout from "./component/PlayerLayout/PlayerLayout";
-import AdminLayout from "./component/AdminLayout/AdminLayout";
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import PlayerLayout from './component/PlayerLayout/PlayerLayout';
+import AdminLayout from './component/AdminLayout/AdminLayout';
+import Particles from './component/Particles';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
+        <Particles />
+        <div className='App'>
           <Switch>
-            <Route exact path="/" component={PlayerLayout} />
-            <Route path="/admin" component={AdminLayout} />
+            <Route exact path='/' component={PlayerLayout} />
+            <Route path='/admin' component={AdminLayout} />
           </Switch>
         </div>
       </BrowserRouter>
