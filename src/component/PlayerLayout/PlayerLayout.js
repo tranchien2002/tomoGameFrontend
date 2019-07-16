80fc38182cdf3f9b67ac7f10eb1bd3a42dfd0484
le.thanh.cong <SUN-ASTERISK\le.thanh.cong@i120917-pc.sun-asterisk.com>
2019-07-16 10:01:06 +0700

    change background

--- src/component/PlayerLayout/PlayerLayout.js
+++ src/component/PlayerLayout/PlayerLayout.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import { connect } from 'react-redux';
-import { firestoreConnect } from 'react-redux-firebase';
-import { compose } from 'redux';
-import QuesArea from './QuesArea';
-import RankArea from './RankArea';
-import store from 'store';
-import * as tomoAction from 'actions/tomoAction';
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
+import QuesArea from "./QuesArea";
+import RankArea from "./RankArea";
+import store from "store";
+import * as tomoAction from "actions/tomoAction";
 
-import '../App.css';
+import "../App.css";
 
 class PlayerLayout extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      account: '0x0',
-      balance: ''
+      account: "0x0",
+      balance: ""
     };
   }
 
@@ -37,44 +37,39 @@ class PlayerLayout extends Component {
     const { rank } = this.props;
     const { question } = this.props;
     const { wincount } = this.props;
-    const { tomo } = this.props;
-    return (
-      <div>
-        <Container>
-          {tomo.web3 ? (
-            question ? (
-              <Row className='set_height'>
-                <QuesArea ques={question} acc={this.props.tomo} />
-                <RankArea rank={rank} wincount={wincount} />
-              </Row>
-            ) : (
-              <Row className='set_height'>
-                <Col className='box_color' xs='8'>
-                  <div className='margin_box '>
-                    <h1> Waiting ...</h1>
-                  </div>
-                </Col>
-                <RankArea rank={rank} />
-              </Row>
-            )
-          ) : (
-            <Row className='set_height'>
-              <Col className='box_color' xs='8'>
-                <div className='margin_box '>
-                  <h1> You need to login metamask ...</h1>
+    if (question != null) {
+      return (
+        <div>
+          <Container>
+            <Row className="set_height">
+              <QuesArea ques={question} acc={this.props.tomo} />
+              <RankArea rank={rank} wincount={wincount} />
+            </Row>
+          </Container>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Container>
+            <Row className="set_height">
+              <Col className="box_color" xs="8">
+                <div className="margin_box ">
+                  <h1> Waiting ...</h1>
                 </div>
               </Col>
               <RankArea rank={rank} />
             </Row>
-          )}
-        </Container>
-      </div>
-    );
+          </Container>
+        </div>
+      );
+    }
   }
 }
 
-const mapStatetoProps = (state) => {
+const mapStatetoProps = state => {
   const question = state.firestore.data.player_question;
+  // console.log(state.tomo)
   return {
     question: question,
     rank: state.rank.ranking,
@@ -87,7 +82,7 @@ export default compose(
   connect(mapStatetoProps),
   firestoreConnect([
     {
-      collection: 'player_question'
+      collection: "player_question"
     }
   ])
 )(PlayerLayout);
