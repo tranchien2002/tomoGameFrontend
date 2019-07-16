69ffaf2356b641934e5efd8161f88291fc9b9bea
le.thanh.cong <SUN-ASTERISK\le.thanh.cong@i120917-pc.sun-asterisk.com>
2019-07-16 10:24:12 +0700

    fix-conflict

--- src/component/PlayerLayout/QuesArea.js
+++ src/component/PlayerLayout/QuesArea.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import { Col, Button } from "reactstrap";
-import { connect } from "react-redux";
-import { compose } from "redux";
-import * as tomoAction from "actions/tomoAction";
-import store from "../../store";
-import { firestoreConnect } from "react-redux-firebase";
-import "../App.css";
+import React, { Component } from 'react';
+import { Col, Button } from 'reactstrap';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import * as tomoAction from 'actions/tomoAction';
+import store from '../../store';
+import { firestoreConnect } from 'react-redux-firebase';
+import '../App.css';
 
 class QuesArea extends Component {
   constructor(props) {
@@ -21,33 +21,32 @@ class QuesArea extends Component {
     let key = Object.keys(this.props.question).slice(-1)[0];
     let qes = this.props.question;
     return (
-      <Col className="box_color" xs="8">
-        <div className="margin_box ">
-          <div className="question">
-            <Col className="user_account">
+      <Col className='box_color' xs='8'>
+        <div className='margin_box '>
+          <div className='question'>
+            <Col className='user_account'>
               <h5>
                 <strong>Your account :</strong> {acc.account}
               </h5>
               <p>
-                <strong>Balance :</strong> {this.props.balance}{" "}
-                <strong>ETH</strong>
+                <strong>Balance :</strong> {this.props.balance} <strong>ETH</strong>
               </p>
             </Col>
-            <Col className="question_box">
-              <div className="question_position">
+            <Col className='question_box'>
+              <div className='question_position'>
                 <h1>{qes[key].question}</h1>
               </div>
             </Col>
           </div>
-          <Col className="question">
-            <div className="answer_position">
+          <Col className='question'>
+            <div className='answer_position'>
               {qes[key].answer.map((item, key) => (
                 <Col key={key}>
                   <Button
-                    onClick={e => this.click(key)}
-                    className="answer_box"
+                    onClick={(e) => this.click(key)}
+                    className='answer_box'
                     outline
-                    color="primary"
+                    color='primary'
                   >
                     {item}
                   </Button>
@@ -61,7 +60,7 @@ class QuesArea extends Component {
   }
 }
 
-const mapStatetoProps = state => {
+const mapStatetoProps = (state) => {
   const question = state.firestore.data.player_question;
   return {
     question: question,
@@ -74,7 +73,7 @@ export default compose(
   connect(mapStatetoProps),
   firestoreConnect([
     {
-      collection: "player_question"
+      collection: 'player_question'
     }
   ])
 )(QuesArea);
